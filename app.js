ea49a1fc2071cfecf1f6c047e8addc524dd7ff54
Ahsan <ahsan.161815@gmail.com>
2023-09-10 11:45:50 +0500

    changes

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const page404Controller = require('./controller/error');
 
-app.set('view engine','ejs');
-app.set('views','views');
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(page404Controller.get404Page);
+app.use(errorController.get404);
 
 app.listen(3000);
